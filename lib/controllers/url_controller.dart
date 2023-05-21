cf94ab244f7a1b69374f377b424b76b4f4223268
frankjmclarke <frankjmclarke@gmail.com>
2023-05-20 19:32:25 -0700

    ChatGPT refactoring

--- lib/controllers/url_controller.dart
+++ lib/controllers/url_controller.dart
@@ -4,121 +4,48 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_starter/models/models.dart';
 import 'package:flutter_starter/ui/auth/auth.dart';
 import 'package:flutter_starter/ui/ui.dart';
-import 'package:flutter_starter/ui/components/components.dart';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 import '../helpers/StringUtil.dart';
 import '../models/url_model.dart';
 
 class UrlController extends GetxController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  Rxn<UrlModel> firebaseUrl = Rxn<UrlModel>();
-  static UrlController to = Get.find();
-  late TextEditingController nameController;
-  late TextEditingController emailController;
-  late TextEditingController passwordController;
   final FirebaseFirestore _db = FirebaseFirestore.instance;
-  Rxn<UrlModelList> firestoreUrlList = Rxn<UrlModelList>();
-  String _sharedText = "";
-  // Since firestoreUrlList is a Rxn<UrlModelList> (Reactive Extension), any
-  // widget that is listening to the urlList stream will be notified of the
-  // change and can update the UI accordingly.
+
+  final Rxn<UrlModel> firebaseUrl = Rxn<UrlModel>();
+  final Rxn<UrlModelList> firestoreUrlList = Rxn<UrlModelList>();
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   final RxBool admin = false.obs;
   StreamSubscription<String>? _textStreamSubscription;
+  String _sharedText = "";
 
-  Stream<UrlModelList> get urlList => firestoreUrlList.map(
-        (urlList) =>
-            UrlModelList(urls: urlList!.urls.isNotEmpty ? urlList.urls : []),
-      );
+  static UrlController get to => Get.find();
 
-  bool containsText(String text) {
-    final List<UrlModel>? urlList = firestoreUrlList.value?.urls;
-    if (urlList != null) {
-      return urlList.any((urlModel) => urlModel.url == text);
-    }
-    return false;
-  }
-
-  void _addTextToListIfUnique() {
-    if (!containsText(_sharedText)) {
-      UrlModelList currentList = firestoreUrlList.value ?? UrlModelList(urls: []);
-      UrlModel newUrlModel = UrlModel(
-        uid: StringUtil.generateRandomId(15),
-        email: '',
-        name: '',
-        url: _sharedText,
-      );
-      currentList.urls.add(newUrlModel);
-      print('currentList ' + currentList.urls.length.toString());
-      firestoreUrlList.value = currentList;
-      insertUrl(newUrlModel);
-    }
-  }
-
+  @override
   void onInit() {
     super.onInit();
-    nameController = TextEditingController();
-    emailController = TextEditingController();
-    passwordController = TextEditingController();
-    fetchUrlList().then((_) {
-      _textStreamSubscription =
-          ReceiveSharingIntent.getTextStream().listen((String text) {
-            _sharedText = text;
-            _addTextToListIfUnique();
-          });
-      //Receive text data when app is closed
-      ReceiveSharingIntent.getInitialText().then((String? text) {
-        if (text != null) {
+    fetchUrlList();
+    _textStreamSubscription =
+        ReceiveSharingIntent.getTextStream().listen((String text) {
           _sharedText = text;
           _addTextToListIfUnique();
-        }
-      });
-      // Complete the future when fetchUrlList() finishes
-      _completer
-          .complete(); //manually complete the future associated with _completer.
-    });
-  }
-
-  Completer<void> _completer = Completer<void>();
-
-// By calling onInitFuture(), you can wait for the completion of the tasks
-// performed in onInit() using the returned future.
-  Future<void> onInitFuture() {
-    return _completer.future;
-  }
-/*
-The initial fetch of the urls collection from Firestore is done using get() method.
-After fetching the initial data, we set the value of firestoreUrlList to the retrieved UrlModelList.
-We then listen to changes in the Firestore collection using the snapshots() method.
-Whenever there's an update in the Firestore collection, the listener is triggered.
-Inside the listener, we update the value of firestoreUrlList with the updated UrlModelList.
-With this update, the UrlListUI widget should now react to changes in
-firestoreUrlList and update the UI accordingly.
- */
-  Future<void> fetchUrlList() async {
-    final snapshot = await _db.collection('urls').get();
-    final urls = snapshot.docs.map((doc) => UrlModel.fromMap(doc.data())).toList();
-    firestoreUrlList.value = UrlModelList(urls: urls);
-    print("fetchUrlList SUCCESS ");
-
-    // Listen for changes in the Firestore collection
-    _db.collection('urls').snapshots().listen((snapshot) {
-      final urls = snapshot.docs.map((doc) => UrlModel.fromMap(doc.data())).toList();
-      firestoreUrlList.value = UrlModelList(urls: urls);
-      print("Firestore collection updated");
+        });
+    ReceiveSharingIntent.getInitialText().then((String? text) {
+      if (text != null) {
+        _sharedText = text;
+        _addTextToListIfUnique();
+      }
     });
   }
 
   @override
   void onReady() async {
-    // Run every time Url List changes
-    // ever(firestoreUrlList, handleUrlListChanged);
-
-    //firestoreUrlList.bindStream(urlList);
-    //insertTestUrl();
     super.onReady();
     fetchUrlList();
   }
@@ -131,130 +58,75 @@ firestoreUrlList and update the UI accordingly.
     super.onClose();
   }
 
-  void handleUrlListChanged(UrlModelList? _firebaseUrlList) async {
-    if (_firebaseUrlList != null) {
-      firestoreUrlList
-          .bindStream(streamFirestoreUrlList());
-      //await isAdmin();
-    }
+  Stream<UrlModelList> get urlList =>
+      firestoreUrlList.map(
+            (urlList) => UrlModelList(urls: urlList?.urls ?? []),
+      );
 
-    if (_firebaseUrlList == null) {
-      print('Send to signin');
-      Get.offAll(SignInUI());
-    } else {
-      Get.offAll(UrlListUI());
+  bool containsText(String text) {
+    final List<UrlModel>? urlList = firestoreUrlList.value?.urls;
+    if (urlList != null) {
+      return urlList.any((urlModel) => urlModel.url == text);
+    }
+    return false;
+  }
+
+  void _addTextToListIfUnique() {
+    if (!containsText(_sharedText)) {
+      final currentList = firestoreUrlList.value ?? UrlModelList(urls: []);
+      final newUrlModel = UrlModel(
+        uid: StringUtil.generateRandomId(15),
+        email: '',
+        name: '',
+        url: _sharedText,
+      );
+      currentList.urls.add(newUrlModel);
+      firestoreUrlList.value = currentList;
+      insertUrl(newUrlModel);
     }
   }
 
-  Stream<UrlModelList> streamFirestoreUrlList() {
-    return _db
-        .doc('/urls/${firebaseUrl.value!.uid}')
-        .snapshots()
-        .map((snapshot) {
-      List<Map> dataList = [snapshot.data()!];
-      return UrlModelList.fromList(dataList);
-    });
-  }
-
-  Future<void> updateUser(BuildContext context, UserModel user, String oldEmail,
-      String password) async {
-    String _authUpdateUserNoticeTitle = 'auth.updateUserSuccessNoticeTitle'.tr;
-    String _authUpdateUserNotice = 'auth.updateUserSuccessNotice'.tr;
+  Future<void> fetchUrlList() async {
     try {
-      showLoadingIndicator();
-      try {
-        await _auth
-            .signInWithEmailAndPassword(email: oldEmail, password: password)
-            .then((_firebaseUser) async {
-          await _firebaseUser.user!
-              .updateEmail(user.email)
-              .then((value) => _updateUserFirestore(user, _firebaseUser.user!));
-        });
-      } catch (err) {
-        print('Caught error: $err');
-        // not yet working, see this issue https://github.com/delay/flutter_starter/issues/21
-        if (err.toString() ==
-            "[firebase_auth/email-already-in-use] The email address is already in use by another account.") {
-          _authUpdateUserNoticeTitle = 'auth.updateUserEmailInUse'.tr;
-          _authUpdateUserNotice = 'auth.updateUserEmailInUse'.tr;
-        } else {
-          _authUpdateUserNoticeTitle = 'auth.wrongPasswordNotice'.tr;
-          _authUpdateUserNotice = 'auth.wrongPasswordNotice'.tr;
-        }
-      }
-      hideLoadingIndicator();
-      Get.snackbar(_authUpdateUserNoticeTitle, _authUpdateUserNotice,
-          snackPosition: SnackPosition.BOTTOM,
-          duration: Duration(seconds: 5),
-          backgroundColor: Get.theme.snackBarTheme.backgroundColor,
-          colorText: Get.theme.snackBarTheme.actionTextColor);
-    } on PlatformException catch (error) {
-      hideLoadingIndicator();
-      print(error.code);
-      String authError;
-      switch (error.code) {
-        case 'ERROR_WRONG_PASSWORD':
-          authError = 'auth.wrongPasswordNotice'.tr;
-          break;
-        default:
-          authError = 'auth.unknownError'.tr;
-          break;
-      }
-      Get.snackbar('auth.wrongPasswordNoticeTitle'.tr, authError,
-          snackPosition: SnackPosition.BOTTOM,
-          duration: Duration(seconds: 10),
-          backgroundColor: Get.theme.snackBarTheme.backgroundColor,
-          colorText: Get.theme.snackBarTheme.actionTextColor);
+      final snapshot = await _db.collection('urls').get();
+      final urls = snapshot.docs.map((doc) => UrlModel.fromMap(doc.data()))
+          .toList();
+      firestoreUrlList.value = UrlModelList(urls: urls);
+
+      _db.collection('urls').snapshots().listen((snapshot) {
+        final urls = snapshot.docs.map((doc) => UrlModel.fromMap(doc.data()))
+            .toList();
+        firestoreUrlList.value = UrlModelList(urls: urls);
+        print("Firestore collection updated");
+      });
+
+      print("fetchUrlList SUCCESS ");
+    } catch (error) {
+      print("Error fetching url list: $error");
     }
   }
 
-  void _updateUserFirestore(UserModel user, User _firebaseUser) {
-    _db.doc('/urls/${_firebaseUser.uid}').update(user.toJson());
-    update();
-  }
-
-  Future<void> sendPasswordResetEmail(BuildContext context) async {
-    showLoadingIndicator();
-    try {
-      await _auth.sendPasswordResetEmail(email: emailController.text);
-      hideLoadingIndicator();
-      Get.snackbar(
-          'auth.resetPasswordNoticeTitle'.tr, 'auth.resetPasswordNotice'.tr,
-          snackPosition: SnackPosition.BOTTOM,
-          duration: Duration(seconds: 5),
-          backgroundColor: Get.theme.snackBarTheme.backgroundColor,
-          colorText: Get.theme.snackBarTheme.actionTextColor);
-    } on FirebaseAuthException catch (error) {
-      hideLoadingIndicator();
-      Get.snackbar('auth.resetPasswordFailed'.tr, error.message!,
-          snackPosition: SnackPosition.BOTTOM,
-          duration: Duration(seconds: 10),
-          backgroundColor: Get.theme.snackBarTheme.backgroundColor,
-          colorText: Get.theme.snackBarTheme.actionTextColor);
-    }
-  }
-
-  Future<void> signOut() {
+  Future<void> signOut() async {
     nameController.clear();
     emailController.clear();
     passwordController.clear();
-    return _auth.signOut();
+    await _auth.signOut();
   }
 
-  // Insert a test UrlModel into Firestore
+// Insert a test UrlModel into Firestore
   UrlModel testUrl = UrlModel(
     uid: StringUtil.generateRandomId(15),
     email: 'testy@testy.com',
     name: 'Testing URL',
     url: 'https://google.com',
   );
+
   Future<void> insertTestUrl() async {
     insertUrl(testUrl);
   }
 
-  Future<void> insertUrl( UrlModel testUrl) async {
+  Future<void> insertUrl(UrlModel testUrl) async {
     try {
-      // Create a new test UrlModel
       // Convert the UrlModel to a JSON map
       Map<String, dynamic> jsonData = testUrl.toJson();
 
@@ -276,6 +148,4 @@ firestoreUrlList and update the UI accordingly.
       print('Error deleting UrlModel: $error');
     }
   }
-
-
 }
--- lib/ui/url_list_ui.dart
+++ lib/ui/url_list_ui.dart
@@ -4,101 +4,86 @@ import '../controllers/url_controller.dart';
 import '../models/url_model.dart';
 
 class UrlListUI extends StatelessWidget {
-  final UrlController myController = Get.put(UrlController());
+  final UrlController urlController = Get.put(UrlController());
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App'),
-        ),
-        body: StreamBuilder<UrlModelList>(
-          stream: myController.urlList, // Use the stream from UrlController
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (snapshot.hasData) {
-              UrlModelList urlList = snapshot.data!; // Access the updated UrlModelList
-              return ListView.builder(
-                itemCount: urlList.urls.length,
-                itemBuilder: (context, index) {
-                  final urls = urlList.urls;
-                  if (index < urls.length) {
-                    final urlModel = urls[index];
-                    return Dismissible(
-                      key: Key(urlModel.uid), // Use a unique key for each item
-                      background: Container(
-                        color: Colors.red, // Customize the background color
-                        alignment: Alignment.centerRight,
-                        padding: EdgeInsets.only(right: 16.0),
-                        child: Icon(
-                          Icons.delete,
-                          color: Colors.white,
-                        ),
-                      ),
-                      confirmDismiss: (direction) async {
-                        return await showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text('Delete Item'),
-                              content: Text('Are you sure you want to delete this item?'),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () => Navigator.of(context).pop(false),
-                                  child: Text('Cancel'),
-                                ),
-                                TextButton(
-                                  onPressed: () => Navigator.of(context).pop(true),
-                                  child: Text('Delete'),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      onDismissed: (direction) {
-                        // Remove the item from the list when dismissed
-                        myController.deleteUrl(urlModel);
-                      },
-                      child: Card(
-                        child: ListTile(
-                          title: Container(
-                            width: MediaQuery.of(context).size.width,
-                            child: Text(
-                              urlModel.url,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          subtitle: Container(
-                            width: MediaQuery.of(context).size.width,
-                            child: Text(
-                              urlModel.name,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  } else {
-                    return ListTile(
-                      title: Text('Data not available'),
-                    );
-                  }
-                },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Obx(
+            () {
+          if (urlController.firestoreUrlList.value == null) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            final List<UrlModel> urls = urlController.firestoreUrlList.value!.urls;
+            if (urls.isEmpty) {
+              return Center(
+                child: Text('No data available'),
               );
             } else {
-              return ListTile(
-                title: Text('No data available'),
+              return ListView.builder(
+                itemCount: urls.length,
+                itemBuilder: (context, index) {
+                  final urlModel = urls[index];
+                  return Dismissible(
+                    key: Key(urlModel.uid),
+                    background: Container(
+                      color: Colors.red,
+                      alignment: Alignment.centerRight,
+                      padding: EdgeInsets.only(right: 16.0),
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                    confirmDismiss: (direction) async {
+                      return await showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Delete Item'),
+                            content: Text('Are you sure you want to delete this item?'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(false),
+                                child: Text('Cancel'),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(true),
+                                child: Text('Delete'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    onDismissed: (direction) {
+                      urlController.deleteUrl(urlModel);
+                    },
+                    child: Card(
+                      child: ListTile(
+                        title: Text(
+                          urlModel.url,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        subtitle: Text(
+                          urlModel.name,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  );
+                },
               );
             }
-          },
-        ),
+          }
+        },
       ),
     );
   }
